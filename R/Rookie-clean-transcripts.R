6c58cdd44eb7ebbdd7157a63f602c796c23cf3cc
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-20 15:42:47 -0700

    Updates based on test2

--- R/Rookie-clean-transcripts.R
+++ R/Rookie-clean-transcripts.R
@@ -1,42 +1,49 @@
 clean_transcripts <- function(tbl) {
+  
+  lookahead <- "(?=[\"$♪\'\\.a-z]|[A-Z][\\s-]|\\d(?![-A-Z#]))"
+  
+  full_pattern <- c(
+    str_c("^[A-Z0-9][A-Z0-9\\s\\.\\-\\'#&,/]+", lookahead),
+    str_c("^ACTOR![A-Z]+", lookahead),
+    str_c("^[A-Z][a-z]{1,2}[A-Z]+", lookahead)
+  ) |> 
+    str_flatten(collapse = "|")
+  
   tbl |> 
     mutate(
-      transcript = str_remove_all(transcript, "\\s*[-–]?\\s*\\[[^]]+\\]\\s*[-–]?\\s*\n*") |>
-        str_remove_all("\\s*[-–]?\\s*\\([^)]+\\)\\s*[-–]?\\s*\n*") |>
+      transcript = transcript |> 
+        str_remove_all("\\s*[-–]?\\s*\\[[^]]+\\]\\s*[-–]?\\s*") |>
+        str_remove_all("\\s*[-–]?\\s*\\([^)]+\\)\\s*[-–]?\\s*") |>
         str_replace_all("\n{3,}", "\n\n") |> 
-        str_replace_all("[^\n]\n{1}(?=[A-Z])", "\n\n") |>
+        str_replace_all("[^\n]\n{1}(?=[^\n])", "\n\n") |>
+        str_replace_all("(\u201c|\u201d)", "\"") |>
+        str_replace_all("\u2018|\u2019", "'") |>
+        str_remove_all("\u2028") |> 
+        # Cleaning needs to go before splitting
         str_split("\n\n")
     ) |> 
     unnest(transcript) |> 
+    filter_out(transcript == "") |> 
     mutate(
-      transcript = str_remove(transcript, "^[\\s\\-–]*"),
       rowID = row_number(),
       type = case_when(
-        str_detect(transcript, "(INT|EXT)\\.") ~ 
-          "scene_heading",
-        str_detect(transcript, "[A-Z]+[:punct:]*[A-Z]+$") ~ "caption",
+        transcript %in% captions ~ "caption",
         str_detect(
-          transcript,
-          "^[A-Z][A-Z]+(?:[,/]\\s*[A-Z]+|\\sand\\s[A-Z]+)+(?=\"|[A-Z\\d]{1}[a-z\\d\'\\s-]+)"
-        ) ~ "dialogue_multi_speaker",
-        str_detect(
-          transcript, "^[A-Z][A-Z\\s]+(?=\"|[A-Z\\d]{1}[a-z\\d\'\\s-]+)"
-        ) ~ "dialogue",
-        TRUE ~ "other"
-      ),
-      speaker = if_else(
-        type == "dialogue" | type == "dialogue_multi_speaker",
-        str_extract(
-          transcript,
-          "^[A-Z][A-Z\\s,/\\.]+(?:\\sand\\s[A-Z]+)*(?=\"|[A-Z\\d\"]{1}[a-z\\d\'\\s-]+)"
-        ),
-        NA_character_
+          transcript, "Previously\\son\\s(\"?The Rookie\"?|THE\\sROOKIE|\\.{3})"
+        ) ~ "other",
+        str_detect(transcript, "(INT|EXT)(\\.|,)") ~ "scene_heading",
+        str_detect(transcript, "PATROL\\sCAR") ~ "scene_heading",
+        str_detect(transcript, "^ACTOR![A-Z]+") ~ "dialogue_recorded",
+        str_detect(transcript, all_caps_pattern) ~ "dialogue",
+        str_detect(transcript, full_pattern) ~ "dialogue"
       ),
       scene = case_when(
-        type == "scene_heading" & str_detect(transcript, "EXT\\.") ~ "EXT",
-        type == "scene_heading" & str_detect(transcript, "INT\\.") ~ "INT"
+        type == "scene_heading" & str_detect(transcript, "EXT") ~ "EXT",
+        type == "scene_heading" & str_detect(transcript, "INT") ~ "INT",
+        type == "scene_heading" & 
+          str_detect(transcript, "PATROL\\sCAR") ~ "shop"
       )
     ) |> 
-    separate_longer_delim(speaker, delim = regex("(,\\s|\\sand\\s|/)"))
+    mutate(line = row_number(), .by = c(season, episode), .after = episode)
 
 }

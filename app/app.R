1bbb27492653e17bad492fe9a8b2e06c10743af2
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-27 00:54:57 -0700

    Initial commit; episode directory

---
+++ app/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(tidyverse)
+library(DT)
+
+library(shiny)
+library(tidyverse)
+library(DT)
+
+tbl_transcripts <- readRDS("transcripts_parquet.rds")
+
+options(DT.options = list(
+  pageLength = 10,
+  lengthMenu = c(5, 10, seq(20, 50, 10))
+))
+
+ui <- fluidPage(
+  titlePanel(h1(em("The Rookie"), " Transcript Analysis")),
+  h2("Episode Directory"),
+  fluidRow(
+    column(6, DTOutput("episodes"))
+  ),
+  titlePanel(""),
+  titlePanel("")
+)
+
+server <- function(input, output, session) {
+  
+  output$episodes <- renderDT(
+    tbl_transcripts |> 
+      tibble() |> 
+      select(ep_number, season, episode, title) |>
+      rename(number = ep_number) |> 
+      rename_with(str_to_title)
+  )
+  
+}
+
+shinyApp(ui, server)

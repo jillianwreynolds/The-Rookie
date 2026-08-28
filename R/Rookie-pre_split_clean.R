8abfa1652a948a449d7d38eb66fe86c602ca6957
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-27 23:26:46 -0700

    when missing, add space between character and (

--- R/Rookie-pre_split_clean.R
+++ R/Rookie-pre_split_clean.R
@@ -15,6 +15,8 @@ pre_split_clean <- function(tbl) {
   tbl |> 
     mutate(
       html = html |> 
+        # when missing, add space between character and (
+        str_replace_all("(?<=\\.)\\(", " (") |> 
         # when missing, add space between ) and next character
         str_replace_all("\\)(?=[A-Z]|\")", ") ") |> 
         # replace "♪ / ♪" with "♪" for separate_longer in clean_transcripts()

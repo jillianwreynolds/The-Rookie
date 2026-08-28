864ace6e745d858d9c309a9e4ebae5bd67a51238
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-27 18:14:49 -0700

    Initial commit

---
+++ R/split_italics_dialogue.R
@@ -0,0 +1,39 @@
+#' Split dialogues from "italics" rows
+#'
+#' When rows are `type == "italics"` but have dialogue after italics, this function inserts a sentinel at the `\\s` boundary and separates the lines at the sentinel. It then updates `type` and `speaker`. Whether dialogue follows is determined by whether the character following `"</i>\\s"` is a capital letter. Speaker is determined by a temporary down-filled `last_dialogue_speaker` column.
+#' @param tbl A data frame.
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+split_italics_dialogue <- function(tbl) {
+  
+  tbl |> 
+    mutate(
+      transcript = if_else(
+        type == "italics",
+        transcript |> str_replace_all("(?<=</i>)\\s(?=[A-Z])", "\u270a"),
+        transcript
+      )
+    ) |> 
+    separate_longer_delim(transcript, "\u270a") |> 
+    mutate(
+      type = type |> replace_when(
+        str_detect(transcript, "<i>", negate = TRUE) ~ "dialogue"
+      ),
+      last_dialogue_speaker = if_else(
+        type == "dialogue",
+        speaker,
+        NA_character_
+      )
+    ) |> 
+    fill(last_dialogue_speaker) |> 
+    mutate(
+      speaker = speaker |> replace_when(
+        when_all(type == "dialogue", is.na(speaker)) ~ last_dialogue_speaker
+      )
+    ) |> 
+    select(-last_dialogue_speaker)
+  
+}

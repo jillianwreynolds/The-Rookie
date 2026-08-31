4424c6abef7f017aa53c812d6501bf8d94f0ed23
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-30 17:33:48 -0700

    V.O. fix

--- R/clean_speaker_names.R
+++ R/clean_speaker_names.R
@@ -12,6 +12,9 @@ clean_speaker_names <- function(tbl) {
   tbl |> 
     mutate(
       html = html |> 
+        # replace [] with () for easier speaker_note extraction
+        str_replace_all("\\[V\\.O\\.\\]", "(V.O.)") |>
+        #
         str_replace_all(coll("(on TV) Bobby: "), "BOBBY (on TV)\n") |>    # 3x6
         str_replace(coll("Man: Don't move."), "MAN\nDon't move.") |>      # 4x13
         str_replace(coll("Man #2: Clear!"), "MAN #2\nClear!") |>          # 4x13
@@ -19,6 +22,5 @@ clean_speaker_names <- function(tbl) {
         str_replace_all(coll("Operator: "), "OPERATOR\n") |>              # 7x15
         str_replace_all(coll("Caller: "), "CALLER\n") |> 
         str_replace(coll("BRADFORD/CHEN: Yeah"), "BRADFORD/CHEN\nYeah")   # 8x4
-      
     )
 }

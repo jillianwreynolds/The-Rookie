a4ed5282e6fd6598f03e8d472d9f5b7e8c8ce353
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-30 19:40:56 -0700

    Moved scene cleaning lines to clean_scene_headings()

--- R/clean_previously.R
+++ R/clean_previously.R
@@ -1,6 +1,6 @@
 #' Clean "Previously on..." lines
 #'
-#' Identifies "Previously on..." lines, specifies scene type, and cleans text for consistency.
+#' Identifies "Previously on..." lines and cleans text for consistency.
 #' @param tbl A table with each episode's transcripts split into lines.
 #'
 #' @returns
@@ -14,14 +14,6 @@ clean_previously <- function(tbl) {
       type = case_when(
         str_detect(transcript, "Previously") &
           str_detect(transcript, "(?i)Rookie")           ~ "previously"
-      ),
-      scene_type = case_when(
-        type == "scene_heading" & str_detect(transcript, "INT") ~ "INT",
-        type == "scene_heading" & str_detect(transcript, "EXT") ~ "EXT",
-        type == "scene_heading" & 
-          str_detect(transcript, "PATROL\\sCAR") ~ "shop",
-        type == "scene_heading" & 
-          str_detect(transcript, "MID-WILSHIRE\\sSTATION") ~ "station",
       ),
       transcript = transcript |> replace_when(
         type == "previously" & !str_detect(transcript, "Feds") 

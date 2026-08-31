a3e892b39f90ce8d5e3a74ba9c60e562cacf13a8
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-30 19:40:36 -0700

    other headings as tribble; specify scene type

--- R/clean_scene_headings.R
+++ R/clean_scene_headings.R
@@ -1,5 +1,6 @@
 #' Identify and clean scene headings
 #'
+#' Identifies scene headings, specifies scene type, and cleans text.
 #' @param tbl A table with each episode's transcript split into lines.
 #'
 #' @returns
@@ -8,32 +9,54 @@
 #' @examples
 clean_scene_headings <- function(tbl) {
   
-  other_scene_headings <- c(
-    "CLIPS FROM SEASON 1, EPISODE 20, \"FREE FALL\" ",
-    "PAYNE'S HOUSE - NIGHT",
-    "AERIAL VIEW OF CARAVAN ESCORTED BY 2 POLICE UNITS",
-    "OUTSIDE, TRAILER PARK",
-    "LOPEZ/EVERS HOME, NIGHT - BEDROOM",
-    "CEMETERY, DAY - REBECCA ARMSTRONG'S GRAVESITE",
-    "RESIDENTIAL STREET, DAY",
-    "NEWS CLIP - DECEMBER 5, 2022, COAST GUARD SEARCH FOR MISSING OFFICER",
-    "VIDEO CALL WITH INTERVIEWER, ABIGAIL, JENSEN ACKLES, JARED PADALECKI"
+  other_scene_headings <- tribble(
+    ~text,      ~type,
+    # 1x7
+    "PAYNE'S HOUSE - NIGHT",                                       "INT",
+    # 2x1
+    "CLIPS FROM SEASON 1, EPISODE 20, \"FREE FALL\"",              "previously",
+    "AERIAL VIEW OF CARAVAN ESCORTED BY 2 POLICE UNITS",           "EXT",
+    # 2x12
+    "OUTSIDE, TRAILER PARK",                                       "EXT",
+    # 3x1
+    "LOPEZ/EVERS HOME, NIGHT - BEDROOM",                           "INT",
+    "CEMETERY, DAY - REBECCA ARMSTRONG'S GRAVESITE",               "EXT",
+    # 4x17
+    "RESIDENTIAL STREET, DAY",                                     "EXT",
+    # 8x15
+    "NEWS CLIP - DECEMBER 5, 2022, COAST GUARD SEARCH FOR MISSING OFFICER","AV",
+    "VIDEO CALL WITH INTERVIEWER, ABIGAIL, JENSEN ACKLES, JARED PADALECKI","AV"
   )
+  
+  prefix_lookup <- setNames(other_scene_headings$type, other_scene_headings$text)
   
   tbl |> 
     mutate(
-      type = case_when(
+      type = type |> replace_when(
         str_detect(transcript, "(INT|EXT)(\\.|,)")       ~ "scene_heading",
         str_detect(transcript, "^(INT|EXT)(\\.|,|\\s)")  ~ "scene_heading",
         str_detect(transcript, "PATROL\\sCAR")           ~ "scene_heading",
-        transcript %in% other_scene_headings             ~ "scene_heading",
+        transcript %in% other_scene_headings$text         ~ "scene_heading",
         str_detect(transcript, "MID-WILSHIRE\\sSTATION") ~ "scene_heading"
+      ),
+      scene_type = case_when(
+        type == "scene_heading" & str_detect(transcript, "INT(\\.|,)") ~ "INT",
+        type == "scene_heading" & str_detect(transcript, "EXT(\\.|,)") ~ "EXT",
+        type == "scene_heading" & 
+          str_detect(transcript, "PATROL\\sCAR") ~ "shop",
+        type == "scene_heading" & 
+          str_detect(transcript, "MID-WILSHIRE\\sSTATION") ~ "station",
       ),
       # Remove parentheses from patrol car scene headings in 4x8
       transcript = if_else(
         str_detect(transcript, "^EXT.+PATROL\\sCAR\\s\\([A-Z]+/[A-Z]+\\)"),
         transcript |> str_replace("\\s\\(", ", ") |> 
           str_remove("\\)$"),
+        transcript
+      ),
+      transcript = if_else(
+        transcript %in% names(prefix_lookup),
+        str_c(prefix_lookup[transcript], ". ", transcript),
         transcript
       )
     )

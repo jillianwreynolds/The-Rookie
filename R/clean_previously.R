4b8af395d777f0b8a57e2457910a22319bea43c6
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-30 18:57:54 -0700

    Initial commit

---
+++ R/clean_previously.R
@@ -0,0 +1,26 @@
+#' Clean "Previously on..." lines
+#'
+#' Identifies "Previously on..." lines and clean text for consistency.
+#' @param tbl A table with each episode's transcripts split into lines.
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+clean_previously <- function(tbl) {
+  
+  tbl |> 
+    mutate(
+      type = type |> replace_when(
+        str_detect(transcript, "Previously") &
+          str_detect(transcript, "(?i)Rookie")           ~ "previously"
+      ) |> 
+      transcript = transcript |> replace_when(
+        type == "previously" & !str_detect(transcript, "Feds") 
+        ~ "Previously on \"The Rookie\"",
+        type == "previously" & str_detect(transcript, "Feds") 
+        ~ "Previously on \"The Rookie\" and \"The Rookie: Feds\""
+      )
+    )
+  
+}

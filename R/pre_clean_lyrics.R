db881dee66a9b341231668b53c0a06c7fbe91a7a
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-30 22:24:57 -0700

    Modified for placement after split_lines()

--- R/pre_clean_lyrics.R
+++ R/pre_clean_lyrics.R
@@ -2,14 +2,13 @@ pre_clean_lyrics <- function(tbl) {
   
   tbl |> 
     mutate(
-      html = html |> 
+      transcript = transcript |> 
         # un-italicize lyrics
         str_remove_all("^<i>(?=\u266a)") |> 
         str_remove_all("(?<=\u266a)</i>") |> 
-        # replace "♪ / ♪" with "♪" for separate_longer in clean_transcripts()
+        # replace "♪ / ♪" with "♪" for separate_longer_delim()
         str_replace_all("\u266a\\s/\\s\u266a", "\u266a") |> 
         # remove trailing ♪ and potential ellipsis
         str_remove("\u266a(\\s\\.{3})?$")
     )
-  
 }

05c10ddbb7df930611a7cb8325d391bd51ec979f
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-31 00:08:05 -0700

    Manual adds

--- R/pre_clean_lyrics.R
+++ R/pre_clean_lyrics.R
@@ -4,11 +4,13 @@ pre_clean_lyrics <- function(tbl) {
     mutate(
       transcript = transcript |> 
         # un-italicize lyrics
-        str_remove_all("^<i>(?=\u266a)") |> 
+        str_remove_all("^<i>(?=\u266a)") |>
         str_remove_all("(?<=\u266a)</i>") |> 
         # replace "♪ / ♪" with "♪" for separate_longer_delim()
-        str_replace_all("\u266a\\s/\\s\u266a", "\u266a") |> 
-        # remove trailing ♪ and potential ellipsis
-        str_remove("\u266a(\\s\\.{3})?$")
+        str_replace_all("\u266a\\s/\\s\u266a", "\u266a \u266a") |> 
+        # remove trailing space and ellipsis
+        str_remove("\\s?(\\.{3})?$") |> 
+        str_replace(coll("♪ La gente paga"), "♪ La gente paga ♪") |> 
+        str_replace(coll("Vuole qua ♪"), "♪ Vuole qua ♪")
     )
 }

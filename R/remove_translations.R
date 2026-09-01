fdb56a0d885ee1b8db1e3a98ed0bcd6dbb0e73be
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-31 19:56:27 -0700

    Initial commit

---
+++ R/remove_translations.R
@@ -0,0 +1,16 @@
+remove_translations <- function(tbl) {
+  
+  tbl |> 
+    mutate(
+      html = html |> 
+        # 1x4
+        str_remove("(?<=<i>quinceañera</i>\\.)\\s\\-\\s15th\\birthday") |> 
+        str_remove("(?<=<i>abuelo</i>\\.)\\s\\-\\sHer\\sdad\\.") |> 
+        # 7x17
+        str_remove("(?<=verruckt)\\s\\(crazy\\)") |> 
+        # 8x11
+        str_remove("(?<=<i>Sumpfland</i>\\.)\\s\\[marshland\\]") |> 
+        str_remove("(?<=Wunderbar)\\s\\(wonderful\\)")
+    )
+  
+}

dc679af4f37152dd2c7153312419801cad296316
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-31 22:53:08 -0700

    Replaced fmt for "Game Changer"

--- R/pre_clean.R
+++ R/pre_clean.R
@@ -23,43 +23,48 @@ pre_clean <- function(tbl) {
         str_remove(
           "^(THE\\sROOKIE[\\s\\S]+?\"[^\"\\n]+\"[^\\S\\n]*\\n+|[\\s\\n]+)"
         ) |> 
+        
         # add/remove/rearrange speaker info
-        str_replace(
+        str_replace( # 1x2
           coll("ANNOUNCER\nPreviously on \"The Rookie\""),
           "Previously on the \"The Rookie\""
         ) |> 
-        str_replace(
-          coll("[ Kai singing low in Italian ]"),
-          "KAI (singing low in Italian)"
-        ) |> 
-        str_replace(
+        str_replace( # 2x18
           coll("[ Gunshots, people screaming ]\nHelp us! He's got a gun!"),
           "[ Gunshots, people screaming ]\nWOMAN\nHelp us! He's got a gun!"
         ) |> 
+        str_replace( # 4x12
+          coll("[ Kai singing low in Italian ]"),
+          "KAI (singing low in Italian)"
+        ) |> 
         
         # typos
-        str_replace(coll("La gente pa♪a"), "La gente paga") |> 
-        str_replace(
+        str_replace( # 1x12
           coll("NOLAND and BEN are putting away the party."),
           "NOLAN and BEN are putting away the party."
         ) |> 
-        str_replace(
+        str_replace(coll("La gente pa♪a"), "La gente paga") |>  # 4x12
+        str_replace( # 5x19
           coll("NOLAND and BAILEY are sitting on the sofa."),
           "NOLAN and BAILEY are sitting on the sofa."
         ) |> 
-        str_replace(
+        str_replace( # 8x10
           coll("barricaded inside a bulding"),
           "barricaded inside a building"
         ) |> 
         
         # punctuation
-        str_replace(
+        str_replace( # 1x1
           coll("[SIREN CHIRPS] - [KNOCK ON DOOR]"),
           "[SIREN CHIRPS] [KNOCK ON DOOR]"
         ) |> 
-        str_replace(
+        str_replace( # 5x11
           coll("(Kyra finally lays down the weapon)."),
           "(Kyra finally lays down the weapon)"
+        ) |> 
+        str_replace( # 8x15
+          coll("cast of</i>Game Changer<i>are trying"),
+          "cast of \"Game Changer\" are trying"
         )
     )
   

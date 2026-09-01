f44e9edd7999a320c29067b1908a15b878effd8c
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-31 18:12:25 -0700

    Rm potential code

--- R/extract_speaker_note.R
+++ R/extract_speaker_note.R
@@ -4,25 +4,7 @@ extract_speaker_note <- function(tbl) {
     mutate(
       # extract from speaker name
       speaker_note = speaker |> str_extract("(?<=[\\[|\\(]).+(?=[\\]|\\)]$)"),
-      speaker = speaker |> str_remove("\\s[\\[|\\(].+[\\]|\\)]$"),
-      #extract from transcript column
-      # speaker_note = speaker_note |> replace_when(
-      #   when_all(is.na(speaker_note), str_detect(transcript, "^(\\(|\\[)")) ~ 
-      #   transcript |> str_extract("(?<=[\\[|\\(]).+(?=[\\]|\\)])")
-      # ) #,
-      # transcript = transcript |> replace_when(
-      #   str_equal(
-      #     speaker_note,
-      #     str_extract(transcript, "(?<=[\\[|\\(]).+(?=[\\]|\\)])")
-      #   ) ~ transcript |> str_remove("[\\[|\\(].+[\\]|\\)]\\s")
-      # )
-      # transcript = transcript |> replace_when(
-      #   when_all(
-      #     !is.na(speaker_note),
-      #     str_detect(transcript, "^(\\(|\\[)"),
-      #     str_detect(transcript, "(?<=[\\[|\\(])sing*(?=[\\]|\\)])")
-      #   ) ~ transcript |> str_remove("[\\[|\\(].+[\\]|\\)]\\s")
-      # )
+      speaker = speaker |> str_remove("\\s[\\[|\\(].+[\\]|\\)]$")
     )
   
 }

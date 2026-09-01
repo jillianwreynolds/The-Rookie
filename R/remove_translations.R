293f343801ca29fdde01fb6c516473f4502f47d5
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-31 20:03:01 -0700

    Fixed typo

--- R/remove_translations.R
+++ R/remove_translations.R
@@ -4,7 +4,7 @@ remove_translations <- function(tbl) {
     mutate(
       html = html |> 
         # 1x4
-        str_remove("(?<=<i>quinceañera</i>\\.)\\s\\-\\s15th\\birthday") |> 
+        str_remove("(?<=<i>quinceañera</i>\\.)\\s\\-\\s15th\\sbirthday\\.") |> 
         str_remove("(?<=<i>abuelo</i>\\.)\\s\\-\\sHer\\sdad\\.") |> 
         # 7x17
         str_remove("(?<=verruckt)\\s\\(crazy\\)") |> 

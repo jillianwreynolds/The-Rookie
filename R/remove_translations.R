69bcf0df42b7c52f0ed446ba130103c2b458b066
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-08-31 21:23:39 -0700

    Rm translations and footnotes from 4x1

--- R/remove_translations.R
+++ R/remove_translations.R
@@ -6,6 +6,19 @@ remove_translations <- function(tbl) {
         # 1x4
         str_remove("(?<=<i>quinceañera</i>\\.)\\s\\-\\s15th\\sbirthday\\.") |> 
         str_remove("(?<=<i>abuelo</i>\\.)\\s\\-\\sHer\\sdad\\.") |> 
+        # 4x1
+        str_remove("(?<=<i>hermana</i>)1") |> 
+        str_remove("(?<=bonita</i>\\.)1") |> 
+        str_remove("(?<=chiquita</i>)3") |> 
+        str_remove("(?<=sicarios</i>)4") |> 
+        str_remove("(?<=exfil)5") |>    # not a translation, just a footnote
+        str_remove("(?<=muerte</i>)6") |> 
+        str_replace("Calma te</i>\\d<i>\\.</i>\\sWe", "Calma te</i>. We") |> 
+        str_remove("(?<=supuesto</i>)8") |> 
+        str_remove(regex(
+          "1\\s\\-\\ssister.+?of\\scourse",
+          dotall = TRUE
+        )) |> 
         # 7x17
         str_remove("(?<=verruckt)\\s\\(crazy\\)") |> 
         # 8x11

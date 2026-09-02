77911ff1bdaa90b1dbf287f0328837832acbb103
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-02 02:53:16 -0700

    Add name, n_episodes; start theme

--- app/app.R
+++ app/app.R
@@ -10,11 +10,20 @@ options(DT.options = list(
   lengthMenu = c(5, 10, seq(20, 50, 10))
 ))
 
+theme <- bslib::bs_theme(
+  bootswatch = "cerulean",
+  primary = "#0a333f"
+)
+
 ui <- fluidPage(
+  theme = theme,
   titlePanel(h1(em("The Rookie"), " Transcript Analysis")),
+  p("By Jillian W. Reynolds", style = "font-size:22px"),
+  titlePanel(""),
   h2("Episode Directory"),
   fluidRow(
-    column(6, DTOutput("episodes"))
+    column(6, DTOutput("episodes")),
+    column(2, DTOutput("n_episodes", width = "10%"))
   ),
   titlePanel(""),
   titlePanel("")
@@ -30,6 +39,17 @@ server <- function(input, output, session) {
       rename_with(str_to_title)
   )
   
+  output$n_episodes <- renderDT(
+    tbl_transcripts |> 
+      select(season, episode) |> 
+      distinct() |>  
+      count(season) |> 
+      collect() |> 
+      rename(Season = season, Episodes = n),
+    options = list(dom = "t")
+  )
+  
 }
 
 shinyApp(ui, server)
+

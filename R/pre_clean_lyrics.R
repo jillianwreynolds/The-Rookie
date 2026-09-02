8af16b3c230e7b858de0ac300367707c966bd963
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-02 00:24:53 -0700

    Added manual fix

--- R/pre_clean_lyrics.R
+++ R/pre_clean_lyrics.R
@@ -10,7 +10,16 @@ pre_clean_lyrics <- function(tbl) {
         str_replace_all("\u266a\\s/\\s\u266a", "\u266a \u266a") |> 
         # remove trailing space and ellipsis
         str_remove("\\s?(\\.{3})?$") |> 
+        
+        # manual fixes
+        str_replace( # 3x1
+          coll("whisper \"I love you\" ♪ Birds singing"),
+          "whisper \"I love you\" ♪ ♪ Birds singing"
+        ) |>
+        # 4x12
         str_replace(coll("♪ La gente paga"), "♪ La gente paga ♪") |> 
-        str_replace(coll("Vuole qua ♪"), "♪ Vuole qua ♪")
+        str_replace(coll("Vuole qua ♪"), "♪ Vuole qua ♪") |> 
+        # 4x17
+        str_replace_all(coll("[sings]"), "(sings)")
     )
 }

4ea05d0626af744637ee028d266a2c25a1cfa824
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-03 03:56:34 -0700

    Temporary About section

--- app/app.R
+++ app/app.R
@@ -51,6 +51,21 @@ ui <- fluidPage(
   p("By Jillian W. Reynolds", style = "font-size:22px"),
   titlePanel(""),
   # tags$details(about_section),
+  fluidRow(
+    column(
+      3,
+      tags$details(
+        tags$summary(
+          "About This Project", style = "display: list-item; font-size:20px"
+        ),
+        p(
+          "This project uses HTML downloads of episode transcripts from ", tags$a(href="https://the-rookie.fandom.com/", "the-rookie.fandom.com", .noWS = "after", target = "_blank"), ". It uses various ", code("R"), " packages and functions to parse the HTML, extract information, and analyze the data. The tables below were not a product of manual typing and counting or copying and pasting information from existing lists. This project is still in progress.",
+          style = "font-size:16px"
+        )
+      )
+    ),
+    column(9)
+  ),
   titlePanel(""),
   h2("Episode Directory"),
   titlePanel(""),

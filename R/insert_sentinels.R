b5e4d20f61a9c46d943f5d4e435bdd31bcd8a56b
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-08 19:08:23 -0700

    Updated 2nd red circle line

--- R/insert_sentinels.R
+++ R/insert_sentinels.R
@@ -12,7 +12,7 @@ insert_sentinels <- function(tbl) {
           # 🔴 before [ or (
           str_replace_all("\\s(?=[\\[|\\(])", red_circle) |> 
           # 🔴 after ] or )
-          str_replace_all("(?<=[\\]|\\)])\\s(?!\u266a)", red_circle) |> 
+          str_replace_all("(?<=[\\]|\\)])\\s", red_circle) |> 
           # 🟫 between lyrics lines " ♪ ♪ "
           str_replace_all("(?<=\u266a)\\s(?=\u266a)", brown_square) |> 
           # 🟤 between dialogue and lyrics

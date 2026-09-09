c4dbe7b78312cc45f182e5a5b39ddf8293e67d44
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-09 01:15:32 -0700

    Initial commit

---
+++ R/unitalicize_descriptions2.R
@@ -0,0 +1,20 @@
+unitalicize_descriptions2 <- function(tbl) {
+  
+  tbl |> 
+    mutate(
+      transcript = if_else(
+        type == "italics",
+        transcript |> 
+          str_replace_all(coll("</i>."), ".<i>") |> 
+          str_replace_all("<i>", "[") |> 
+          str_replace_all("</i>", "]"),
+        transcript
+      ),
+      type = if_else(
+        str_detect(transcript, "^\\["),
+        "description",
+        type
+      )
+    )
+  
+}

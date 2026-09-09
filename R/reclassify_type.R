72b66b03ea2749457ec7a0681185e7e08402ee47
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-08 18:05:55 -0700

    Initial commit

---
+++ R/reclassify_type.R
@@ -0,0 +1,23 @@
+reclassify_type <- function(tbl) {
+  
+  tbl |> 
+    mutate(
+      type = if_else(
+        str_detect(transcript, "^[\\[\\(]"),
+        "description",
+        type
+      ),
+      type = if_else(
+        when_all(
+          type == "italics",
+          str_detect(transcript, "^[A-Z\"]")
+        ),
+        "dialogue",
+        type
+      ),
+      dialogue_type = case_when(
+        str_detect(transcript, "\u266a") ~ "lyrics"
+      )
+    )
+  
+}

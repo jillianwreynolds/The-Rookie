92e81e6cec41921bdb97b1bf34787e95cdd73c8e
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-10 13:32:07 -0700

    Mv code for main/recurring/status tables to functions and targets pipeline

--- _targets.R
+++ _targets.R
@@ -27,6 +27,22 @@ token_values <- expand_grid(
   )
 
 list(
+  tar_target(main_chars_csv, "characters.csv", format = "file"),
+  tar_target(recurring_chars_csv, "recurring_characters.csv", format = "file"),
+  tar_target(main_chars_wide, create_main_chars_wide(main_chars_csv)),
+  tar_target(main_chars_long, create_main_chars_long(main_chars_wide)),
+  tar_target(
+    recurring_chars_wide, create_recurring_chars_wide(recurring_chars_csv)
+  ),
+  tar_target(
+    recurring_chars_long, create_recurring_chars_long(recurring_chars_wide)
+  ),
+  tar_target(
+    char_status_long,
+    create_char_status_long(main_chars_long, recurring_chars_long)
+  ),
+  tar_target(char_status_wide, create_char_status_wide(char_status_long)),
+  tar_target(characters, create_characters_tbl(char_status_long)),
   tar_target(
     transcript_list,
     list.files("data/", pattern = "\\.pdf$", recursive = TRUE)
@@ -43,8 +59,8 @@ list(
   tar_target(
     transcripts_parquet,
     {
-      transcripts |> 
-        select(ep_number, season, episode, title) |> 
+      transcripts |>
+        select(ep_number, season, episode, title) |>
         write_parquet("data/transcripts_parquet.parquet")
       "data/transcripts_parquet.parquet"
     },
@@ -53,7 +69,7 @@ list(
   tar_target(
     df_raw,
     {
-      transcripts |> 
+      transcripts |>
         run_cleaning_pipe() |>
         write_parquet("data/df_raw.parquet")
       "data/df_raw.parquet"
@@ -63,7 +79,7 @@ list(
   tar_target(
     df_clean,
     {
-      transcripts |> 
+      transcripts |>
         run_cleaning_pipe(clean = TRUE) |>
         write_parquet("data/df_clean.parquet")
       "data/df_clean.parquet"

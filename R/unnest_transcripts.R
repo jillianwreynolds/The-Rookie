6ea486a0cfa50acc544911f0810ddc9edd7940a1
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-09 22:08:29 -0700

    Relocate cols; separate word col when n is 2 or 3

--- R/unnest_transcripts.R
+++ R/unnest_transcripts.R
@@ -8,7 +8,17 @@ unnest_transcripts <- function(tbl, token = "words", n = NULL) {
     )
   }
   
-  tbl |> 
-    mutate(word = unnested |> str_to_lower(), .after = unnested)
+  tbl <- tbl |> 
+    mutate(word = unnested |> str_to_lower()) |> 
+    relocate(c(unnested, word), .after = speaker_note)
+  
+  if (token == "words") {
+    tbl
+  } else {
+    names_2 <- c("word1", "word2")
+    names_3 <- c("word1", "word2", "word3")
+    name_vec <- if(n == 2) names_2 else names_3
+    tbl |> separate_wider_delim(word, delim = " ", names = name_vec)
+  }
   
 }

2eb124e0e0688f6f684ab865e798c949b8c046bb
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-09 21:27:27 -0700

    Added lines

--- helpers/targets-helpers.R
+++ helpers/targets-helpers.R
@@ -1,6 +1,9 @@
 tar_edit()
 
+tar_outdated()
+
 tar_visnetwork()
+tar_visnetwork(targets_only = TRUE)
 tar_visnetwork(physics = TRUE)
 
 tar_make()
@@ -11,3 +14,5 @@ tar_meta(fields = error, complete_only = TRUE)
 tar_meta(fields = name)
 tar_meta(fields = name) |> print_inf()
 tar_meta(fields = name) |> filter(str_detect(name, ""))
+
+tar_manifest()

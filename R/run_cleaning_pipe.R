a39509fedb31e6908bb8c9cfbaa1e7892ffd48a1
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-09 19:12:23 -0700

    Initial commit; WIP test clean_multispeaker_names() at end

---
+++ R/run_cleaning_pipe.R
@@ -0,0 +1,35 @@
+run_cleaning_pipe <- function(tbl, clean = FALSE) {
+  
+  tbl <- tbl |> 
+    pre_clean() |> 
+    clean_speaker_names() |>
+    remove_translations() |> 
+    add_brackets() |>
+    replace_italics() |> 
+    add_space()
+  
+  if (clean) {
+    tbl <- tbl |> clean_proper_nouns()
+  }
+  
+  tbl |> 
+    split_transcripts() |>
+    shift_italics_tags() |>
+    clean_previously() |> 
+    clean_scene_headings() |> 
+    clean_captions() |> 
+    clean_other() |> 
+    pre_clean_lyrics() |> 
+    assign_speaker_names() |> 
+    extract_speaker_note() |> 
+    insert_sentinels() |> 
+    unitalicize_descriptions() |> 
+    unify_sentinels() |> 
+    separate_at_sentinels() |> 
+    reclassify_type() |>
+    fill_speaker_info() |>
+    clean_multispeaker_names() |> 
+    unitalicize_descriptions2() |> 
+    replace_italics2()
+  
+}

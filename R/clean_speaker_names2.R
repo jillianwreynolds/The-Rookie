dee624316cac996928f2c484a45541092c6ee206
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-10 21:46:28 -0700

    Added episode-specific replacements

--- R/clean_speaker_names2.R
+++ R/clean_speaker_names2.R
@@ -31,34 +31,55 @@ clean_speaker_names2 <- function(tbl) {
         ),
         sp3
       ),
+      # RACHEL HALL
+      sp4 = if_else(
+        when_all(is.na(sp3), season != 1, episode != 11),
+        sp4 |> replace_when(sp4 == "RACHEL" ~ "HALL"),
+        sp4
+      ),
+      sp3 = if_else(
+        when_all(is.na(sp3), season != 1, episode != 11),
+        sp3 |> replace_when(sp4 == "HALL" ~ "RACHEL"),
+        sp3
+      ),
+      # COLIN HALL
+      sp3 = if_else(
+        when_all(is.na(sp3), season == 2, episode == 15),
+        sp3 |> replace_when(sp4 == "COLIN" ~ "COLIN"),
+        sp3
+      ),
+      sp4 = if_else(
+        when_all(season == 2, episode == 15),
+        sp4 |> replace_when(sp4 == "COLIN" ~ "HALL"),
+        sp4
+      ),
+      # TOM BRADFORD
       sp4 = if_else(
         when_all(is.na(sp3), season == 4, episode == 9),
-        sp4 |> replace_when(
-          sp4 == "TOM" ~ "BRADFORD"
-        ),
+        sp4 |> replace_when(sp4 == "TOM" ~ "BRADFORD"),
         sp4
       ),
       sp3 = if_else(
         when_all(is.na(sp3), season == 4, episode == 9),
-        sp3 |> replace_when(
-          sp4 == "BRADFORD" ~ "TOM"
-        ),
+        sp3 |> replace_when(sp4 == "BRADFORD" ~ "TOM"),
         sp3
       ),
+      # LINCOLN THORSEN
       sp4 = if_else(
         when_all(is.na(sp3), season == 4, episode == 16),
-        sp4 |> replace_when(
-          sp4 == "LINCOLN" ~ "THORSEN"
-        ),
+        sp4 |> replace_when(sp4 == "LINCOLN" ~ "THORSEN"),
         sp4
       ),
       sp3 = if_else(
         when_all(is.na(sp3), season == 4, episode == 16),
-        sp3 |> replace_when(
-          sp4 == "THORSEN" ~ "LINCOLN"
-        ),
+        sp3 |> replace_when(sp4 == "THORSEN" ~ "LINCOLN"),
         sp3
-      )
+      ),
+      # 5x9 "NYPD SMITTY" as moniker
+      sp4 = if_else(
+        when_all(sp3 == "NYPD", sp4 == "SMITTY"), "NYPD_SMITTY", sp4
+      ),
+      sp3 = if_else(sp4 == "NYPD_SMITTY", NA_character_, sp3)
     )
   
 }

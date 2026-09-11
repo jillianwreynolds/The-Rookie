226f8042af748f39c66557b4f03577808148ea2e
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-10 21:17:45 -0700

    Mv case check logic to check_name_variations()

--- R/check_name_variations.R
+++ R/check_name_variations.R
@@ -1,5 +1,9 @@
 check_name_variations <- function(tbl = df_raw, pattern) {
   
+  if (!str_detect(pattern, "Ma?c[A-Z]") && str_detect(pattern, "[a-z]")) {
+    pattern <- pattern |> str_to_upper()
+  }
+  
   tbl |> 
     select(speaker) |> 
     filter(str_detect(speaker, pattern)) |> 
--- R/check_names.R
+++ R/check_names.R
@@ -13,10 +13,6 @@
 #' }
 check_names <- function(tbl, pattern, peek = TRUE, print_inf = TRUE) {
   
-  if (!str_detect(pattern, "Ma?c[A-Z]") && str_detect(pattern, "[a-z]")) {
-    pattern <- pattern |> str_to_upper()
-  }
-  
   print_tibble <- tbl |> check_name_variations(pattern)
   
   if (print_inf) {

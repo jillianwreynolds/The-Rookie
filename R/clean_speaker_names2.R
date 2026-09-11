0621c0c90b9e174e0aa7897894e28a8a2b6e2492
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-10 21:19:17 -0700

    Added more 2-sided formulas

--- R/clean_speaker_names2.R
+++ R/clean_speaker_names2.R
@@ -4,11 +4,12 @@ clean_speaker_names2 <- function(tbl) {
     mutate(
       sp4 = if_else(
         is.na(sp3),
-        # put last name in sp4
+        # replace first name with last name
         sp4 |> replace_when(
           sp4 == "ANGELA" ~ "LOPEZ",
+          sp4 == "BAILEY" ~ "NUNE",
           sp4 == "TIM"    ~ "BRADFORD",
-          sp4 == "BAILEY" ~ "NUNE"
+          sp4 == "WESLEY" ~ "EVERS"
         ),
         sp4
       ),
@@ -18,9 +19,13 @@ clean_speaker_names2 <- function(tbl) {
           # add first name in sp3
           sp4 == "ANDERSEN" ~ "ZOE",
           sp4 == "BRADFORD" ~ "TIM",
+          sp4 == "CHEN"     ~ "LUCY",
+          sp4 == "HARPER"   ~ "NYLA",
+          sp4 == "NOLAN"    ~ "JOHN",
           sp4 == "RUSSO"    ~ "JESSICA",
-          # update sp3 with first name
+          # add first name (sp4 originally first name)
           sp4 == "BRADFORD" ~ "TIM",
+          sp4 == "EVERS"    ~ "WESLEY",
           sp4 == "LOPEZ"    ~ "ANGELA",
           sp4 == "NUNE"     ~ "BAILEY"
         ),

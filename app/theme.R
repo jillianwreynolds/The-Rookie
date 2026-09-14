7c4105404b9b785a781b48cc1ab6db75fb43628c
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-13 21:13:54 -0700

    Initial commit

---
+++ app/theme.R
@@ -0,0 +1,9 @@
+theme <- bs_theme(
+  primary = "#0a333f",
+  secondary = "#2f8b9d"
+) |>
+  bs_add_variables(
+    "headings-color" = "#0a333f",
+    "link-color" = "#2f8b9d",
+    "code-color" = "purple"
+  )

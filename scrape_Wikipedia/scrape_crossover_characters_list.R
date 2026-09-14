481f461a7abe291b3b74cbc8ca21e8e0f5ebd8e2
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-14 11:48:01 -0700

    Fixed typo

--- scrape_Wikipedia/scrape_crossover_characters_list.R
+++ scrape_Wikipedia/scrape_crossover_characters_list.R
@@ -1,9 +1,9 @@
 # originally scraped on September 14, 2026 between 9 and 10 am
 
-crossover_charachters_text <- "https://en.wikipedia.org/wiki/The_Rookie" |> 
+crossover_characters_text <- "https://en.wikipedia.org/wiki/The_Rookie" |> 
   read_html() |> 
   html_elements("#mwAjg") |> 
   html_text()
 
-crossover_charachters_text |> 
-  write_file("data/Wikipedia/crossover_characters_list.txt")
+crossover_characters_text |> 
+  write_file("data/Wikipedia/crossover_characters.txt")

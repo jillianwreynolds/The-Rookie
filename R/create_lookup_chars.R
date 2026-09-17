e0a472422942214fb9a16c390f2c2ef2056f78dd
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-17 16:24:06 -0700

    Added lookup tables from vars script

--- R/create_lookup_chars.R
+++ R/create_lookup_chars.R
@@ -1,5 +1,67 @@
 create_lookup_chars <- function(chars_data) {
   
+  move_first_add_last_tbl <- tibble(name = c(
+    "ABIGAIL", "ABRIL",
+    "BAILEY", "BEN", "BLANCA",
+    "DIEGO", "DOMINIQUE", "DONOVAN",
+    "ELIJAH", "EMMETT",
+    "GENNIFER", "GENNY", "GRACE",
+    "ISABEL",
+    "JACKSON", "JESSICA", "JOY",
+    "KARLA",
+    "LILA", "LUNA",
+    "NELL",
+    "OSCAR",
+    "PERCY", "PETE",
+    "RANDY", "RODGE", "ROSALIND", "RUBEN",
+    "TAMARA", "TIM",
+    "VIVIAN",
+    "WESLEY",
+    "YVONNE"
+  ))
+  
+  add_first_tbl <- tibble(name = c(
+    "ANDERSEN", "ARMSTRONG",
+    "BISHOP", "BRADFORD",
+    "CHEN", "COLINS",
+    "DE_LA_CRUZ", "DEL_MONTE", "DERIAN", "DYER",
+    "ECKERT",
+    "FREEMAN",
+    "GLASSER", "GREY",
+    "HARPER", "HUTCHINSON",
+    "JUAREZ",
+    "LANG", "LONDON", "LOPEZ",
+    "NOLAN",
+    "PENN",
+    "RIDLEY", "RUSSO",
+    "SANFORD", "SAWYER", "SMITTY", "STANTON", "STONE",
+    "THORSEN",
+    "VESTRI",
+    "WALSH", "WEST", "WOLFE", "WYLER"
+  ))
+  
+  additional_chars <- tribble(
+    ~name,       ~replacement,
+    # move first and add last
+    "GENNIFER",  "BRADFORD",
+    "JOY",       "BRADFORD",
+    "SIMONE",    "CLARK",
+    "DIEGO",     "DE_LA_CRUZ",
+    "JACK",      "EVERS",
+    "PATRICE",   "EVERS",
+    "DOMINIQUE", "GREY",
+    "BLANCA",    "JUAREZ",
+    "KARLA",     "JUAREZ",
+    "BENNY",     "LOPEZ",
+    "DAMIEN",    "LOPEZ",
+    "PETE",      "NOLAN",
+    "YVONNE",    "THORSEN",
+    # add first name
+    "ACRES",     "BRENDAN",
+    "HOPE",      "CARTER",
+    "STENSEN",   "LAURA"
+  )
+  
   bind_rows(
     move_first_add_last_tbl |> mutate(type = "move_first_add_last"),
     add_first_tbl |> mutate(type = "add_first")

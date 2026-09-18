4f05b2b9f91add51c5e819c7f8ba009c3f523f0f
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-17 19:51:59 -0700

    Relocated code, capitalized names, exclude alias/text

--- R/create_notable_guest_chars.R
+++ R/create_notable_guest_chars.R
@@ -3,31 +3,28 @@ create_notable_guest_chars <- function(file_path) {
   file_path |> 
     read_file() |> 
     str_remove_all("\\[.+?\\]") |> 
+    str_remove_all("Dr\\. ") |> 
+    str_replace(
+      coll("Jeffrey Boyle / Eli Reynolds"),
+      "Eli Reynolds / Jeffrey Boyle"
+    ) |> 
     str_split("\n") |> 
     unlist() |> 
     tibble(text = _) |> 
     filter_out(str_detect(text, coll("as Jake Butler and Sava Wu"))) |> 
     mutate(
       actor = text |> str_extract(".+?(?=\\sas\\s)"),
-      character = case_when(
-        str_detect(text, "as\\s(her|him)self") ~ actor
-      ),
-      character = if_else(
-        is.na(character),
-        text |> str_extract("(?<=\\sas\\s).+(?=[:])"),
-        character
-      ),
-      character = character |> str_replace(
-        coll("Jeffrey Boyle / Eli Reynolds"),
-        "Eli Reynolds / Jeffrey Boyle"
-      ),
+      character = text |> str_extract("(?<=\\sas\\s).+(?=[:])"),
+      character = character |> 
+        replace_when(str_detect(text, "(her|him)self") ~ actor) |> 
+        str_to_upper(),
       .before = text
     ) |> 
     separate_wider_delim(
       character, delim = " / ", names = c("character", "alias"),
       too_few = "align_start",
       cols_remove = FALSE
-    ) |>
+    ) |> 
     mutate(
       alias = alias |> replace_when(
         str_detect(character, "\"") ~ str_extract(character, "(?<=\").+(?=\")")
@@ -39,9 +36,10 @@ create_notable_guest_chars <- function(file_path) {
     separate_wider_delim(
       character,
       delim = " ",
-      names = c("char_title", "first_name", "last_name"),
+      names = c("first_name", "last_name"),
       too_few = "align_end"
-    ) |>
-    select(-text)
+    ) |> 
+    select(-c(alias, text)) |> 
+    relocate(ends_with("name"))
   
 }

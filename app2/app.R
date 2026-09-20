6d87133d1326d06140fbd48c44faae229760c9c9
Jillian Reynolds <jillianwreynolds@icloud.com>
2026-09-19 21:01:13 -0700

    shinylive success: loading data in server, colors and bs theme in separate scripts

--- app2/app.R
+++ app2/app.R
@@ -2,66 +2,59 @@ library(shiny)
 library(tidyverse)
 library(DT)
 library(bslib)
+library(nanoparquet)
 
-# setup -------------------------------------------------------------------
-
-c(
-  "R/theme.R",
-  "R/utils.R",
-  "R/home_panel.R",
-  "R/characters_panel.R"
-) |> 
-  walk(\(x) source(x))
-
-tbl_transcripts <- nanoparquet::read_parquet("data/transcripts_parquet.parquet")
-tbl_characters <- nanoparquet::read_parquet("data/characters_parquet.parquet")
+source("colors.R")
+source("my_theme.R")
 
 # UI ----------------------------------------------------------------------
 
-ui <- page_navbar(
-  theme = theme,
-  fillable = FALSE,
-  navbar_options = navbar_options(bg = dark_teal),
-  title = h1(em("The Rookie"), " Transcript Analysis", style = "color:#fff"),
-  nav_spacer(),
-  home_panel,
-  characters_panel
+ui <- fluidPage(
+  theme = my_theme,
+  h1("Title here", style = glue::glue("color:{yellow}")),
+  p("Some UI here"),
+  tableOutput("episodes"),
+  tableOutput("characters"), 
+  tableOutput("ratings"),
+  p("Some more UI here")
 )
 
 # Server ------------------------------------------------------------------
 
 server <- function(input, output, session) {
   
-  output$episodes <- renderDT(
-    tbl_transcripts |> 
-      tibble() |> 
-      select(ep_number, season, episode, title) |>
-      rename(number = ep_number) |> 
-      rename_with(str_to_title),
-    server = FALSE,
-    options = list(
-      pageLength = 10,
-      lengthMenu = c(5, 10, seq(20, 40, 10))
+  my_data <- reactive({
+    withProgress(
+      {
+        tbl_episodes <- nanoparquet::read_parquet("transcripts_parquet.parquet")
+  #       # Sys.sleep(3)
+        incProgress(1 / 4)
+        
+        tbl_characters <- nanoparquet::read_parquet("characters_parquet.parquet")
+        incProgress(1 / 4)
+
+        tbl_ratings <- nanoparquet::read_parquet("episode_ratings.parquet")
+        incProgress(1 / 4)
+        
+        tbl_raw <- nanoparquet::read_parquet("df_raw.parquet")
+
+        list(
+          # episodes = datasets::mtcars
+          episodes = tbl_episodes,
+          characters = tbl_characters,
+          ratings = tbl_ratings,
+          df_raw = tbl_raw
+        )
+      },
+      message = "Loading data...",
+      detail = "This may take several seconds...",
+      value = 0
     )
-  )
+  })
   
-  output$n_episodes <- renderTable(
-    tbl_transcripts |>
-      select(season, episode) |>
-      count(season) |>
-      rename(Season = season, Episodes = n)
-  )
-  
-  output$characters_tbl <- renderDT(
-    tbl_characters |> 
-      select(1:2) |> 
-      mutate(across(
-        everything(),
-        \(x) x |> str_replace_all("_", " ") |>  str_to_title()
-      )) |> 
-      clean_col_names()
-  )
-  
+  output$episodes <- renderTable(my_data()$episodes |> head())
+  output$characters <- renderTable(my_data()$characters |> head())
+  output$ratings <- renderTable(my_data()$ratings |> head())
 }
 
 shinyApp(ui, server)
